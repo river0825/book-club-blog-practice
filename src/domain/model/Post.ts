62c83d09a9e5107155e4e7199db4525a53acbbe7
fong <fong@meepshop.com>
2021-04-30 15:42:32 +0800

    PublishPost: use case and model creation

--- src/domain/model/Post.ts
+++ src/domain/model/Post.ts
@@ -10,6 +10,12 @@ export class Post {
     private _body: string;
     
     constructor(subject: string, body: string) {
+        if (!subject || subject === '') {
+            throw new Error('Post subject should not be empty');
+        }
+        if (!body || body === '') {
+            throw new Error('Post body should not be empty');
+        }
         this._subject = subject;
         this._body = body;
     }
--- test/application/PostApplication.test.ts
+++ test/application/PostApplication.test.ts
@@ -1,10 +1,26 @@
 import {PostApplication} from "../../src/application/PostApplication";
 
-describe('...', () => {
-    test('create post id', () => {
-        let app = new PostApplication()
+describe('Given an author', () => {
+    let app: PostApplication;
+    beforeEach(() => {
+        app = new PostApplication()
+    })
+    test('should get a post if publish a post with correct params', () => {
         let post = app.publishPost("subject", "body")
         expect(post.subject).toBe("subject")
         expect(post.body).toBe("body")
     })
+
+    test('should fail if publish a post with empty subject', () => {
+        expect(() => {
+            app.publishPost("", "body")
+        }).toThrow('Post subject should not be empty');
+    })
+
+    test('should fail if publish a post with empty body', () => {
+        expect(() => {
+            app.publishPost("subject", "")
+        }).toThrow('Post body should not be empty');
+    })
+
 })
